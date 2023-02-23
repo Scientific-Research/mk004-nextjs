cc4a8f946c30df8f03b131eaedcccf2989be9a84
Maximilian <scientificresearch309@gmail.com>
2023-02-23 23:38:41 +0100

    tailwind : text-green-600

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
 function Home() {
-  return <div>This is the home page!nnn123456</div>;
+  return <div className="text-green-600">This is the home page!nnn123456</div>;
 }
 export default Home;
