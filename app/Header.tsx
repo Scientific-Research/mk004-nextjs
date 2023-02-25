180a88ccba0a2a29a06ffce4f2cb5fc3d47eb847
Maximilian <scientificresearch309@gmail.com>
2023-02-25 15:43:01 +0100

    bg-orange-500 and bg-green-500 were added!

--- app/Header.tsx
+++ app/Header.tsx
@@ -5,13 +5,13 @@ function Header() {
   return (
     <header className="p-5 bg-blue-500">
       <Link
-        className="mr-2 px-2 py-1 bg-white text-blue-500 rounded-lg"
+        className="mr-2 px-2 py-1 bg-orange-500 text-blue-500 rounded-lg"
         href="/"
       >
         Home
       </Link>
       <Link
-        className="mr-2 px-2 py-1 bg-white text-blue-500 rounded-lg"
+        className="mr-2 px-2 py-1 bg-green-500 text-blue-500 rounded-lg"
         href="/nouns"
       >
         Nouns
